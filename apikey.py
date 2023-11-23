1f7743849850af21bc417d9bd69f5bb38ee4c7bd
Shivtej Mishra <mishrashivtej54@gmail.com>
2023-11-24 01:02:57 +0530

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,1 +1,2 @@
-apikey = 'AIzaSyCO6WmwD6kZ_JoUNhCZS7IZ4G5pzfvylf4'// This api key will deleted after some time kindly replace With your API key.
+apikey = 'AIzaSyCO6WmwD6kZ_JoUNhCZS7IZ4G5pzfvylf4'
+/*This api key will deleted after some time kindly replace With your api key.*/
